103a4812aab0aa657a906453340299a15cbce472
TASCer <todd@tascs.net>
2024-01-16 12:32:34 -0700

    WIP

--- src/create_service.py
+++ src/create_service.py
@@ -12,30 +12,36 @@ logger = logging.getLogger(__name__)
 def my_selenium_firefox():
 	"""Create and return a selenium Firefox service to be used on pages and forms"""
 	try:
-		ser = Service(f"{my_secrets.firefox_driver}")
-		op = webdriver.FirefoxOptions()
+		service = Service(f"{my_secrets.firefox_driver}")
+		options = webdriver.FirefoxOptions()
 
-		logger.info(f"selenium FIREFOX service created with options: {op}")
+		options.add_argument("--start-maximized")
 
-		return webdriver.Chrome(service=ser, options=op)
+		logger.info(f"selenium FIREFOX service created with options: {options}")
 
-	except (FileNotFoundError, WebDriverException) as e:
-		logger.critical(f"{str(e)}")
-		exit()
+		ff_browser = webdriver.Firefox(service=service, options=options)
 
-
-def my_selenium_chrome():
-	"""Create and return a selenium Firefox service to be used on pages and forms"""
-	try:
-		service = Service(f"{my_secrets.chrome_driver}")
-		options = webdriver.ChromeOptions()
-		options.add_argument("--remote-allow-origins=*")
-		options.add_argument("--disable notifications")
-
-		logger.info(f"selenium CHROME service created with options: {options}")
-
-		return webdriver.Chrome(service=service, options=options)
+		return ff_browser
 
 	except (FileNotFoundError, WebDriverException) as e:
 		logger.critical(f"{str(e)}")
 		exit()
+
+
+# def my_selenium_chrome():
+# 	"""Create and return a selenium Firefox service to be used on pages and forms"""
+# 	try:
+# 		service = Service(f"{my_secrets.chrome_driver}")
+# 		options = webdriver.ChromeOptions()
+# 		options.add_argument("--remote-allow-origins=*")
+# 		options.add_argument("--disable notifications")
+#
+# 		logger.info(f"selenium CHROME service created with options: {options}")
+#
+# 		chr_browser = webdriver.Chrome(service=service, options=options)
+#
+# 		return chr_browser
+#
+# 	except (FileNotFoundError, WebDriverException) as e:
+# 		logger.critical(f"{str(e)}")
+# 		exit()
