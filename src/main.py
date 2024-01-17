275ad828e65d2cd548cd18ab7cc9a50c04238b5b
TASCer <todd@tascs.net>
2024-01-17 11:00:35 -0700

    WIP

--- src/main.py
+++ src/main.py
@@ -1,39 +1,44 @@
 import complete_forms
 import create_service
 import datetime as dt
-import get_page_links
+import test_nav_bar
 import logging
 import my_secrets
+
+from logging import Logger, Formatter
 
 now: dt = dt.date.today()
 todays_date: str = now.strftime('%D').replace('/', '-')
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
+# LOGGING
+root_logger: Logger = logging.getLogger()
+root_logger.setLevel(logging.INFO)
 fh = logging.FileHandler(f'./{todays_date}.log')
 fh.setLevel(logging.DEBUG)
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
-logger.addHandler(fh)
+root_logger.addHandler(fh)
+logger: Logger = logging.getLogger(__name__)
 
-# CHROME = create_service.my_selenium_chrome()
-TEST_FORMS: bool = False
+MAIL_TEST: bool = True
 
 if __name__ == "__main__":
 	logger.info("STARTED WEBSITE TESTING...")
 	FIREFOX = create_service.my_selenium_firefox()
-	logger.info(f"SELENIUM SERVICE CREATED FOR: FIREFOX {type(FIREFOX)}")
-	if TEST_FORMS:
-		contact = complete_forms.contact_form(FIREFOX)
-		logger.info(f"{contact}")
+	logger.info(f"SELENIUM SERVICE CREATED FOR: {type(FIREFOX)}")
+	# if TEST_FORMS:
+	# 	contact = complete_forms.contact_form(FIREFOX)
+	# 	logger.info(f"{contact}")
 		# complete_forms.consult_forms(browser) # CAPTCHA
 		# logger.info(f"{consult}")
 	# browser.get(my_secrets.test_consult_url)
 	# site = browser.current_url
-	else:
-		home_page_links = get_page_links.collect(FIREFOX)
-		FIREFOX.quit()
+	# else:
+	home_page_links = test_nav_bar.collect(FIREFOX, MAIL_TEST)
+	# logger.info(f"Navigation Bar links tested. Mail test: {MAIL_TEST}")
+	FIREFOX.quit()
+		# test_home_links.complete(FIREFOX)
+		# print(home_page_links)
+
 
 
---
+++ src/test_nav_bar.py
@@ -0,0 +1,55 @@
+import datetime as dt
+import logging
+import my_secrets
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+now: dt = dt.date.today()
+todays_date: str = now.strftime('%D').replace('/', '-')
+
+
+# browser = create_service.my_selenium_chrome()
+# logger.info("SELENIUM SERVICE CREATED FOR: FIREFOX")
+
+def collect(browser, MAIL_TEST):
+	logger = logging.getLogger(__name__)
+	logger.info("Testing navigation bar links")
+	browser.get(my_secrets.test_home_url)
+	WebDriverWait(browser, 1000)
+	browser.find_element(By.LINK_TEXT, "WHY TASCS?").click()
+	WebDriverWait(browser, 1000)
+	browser.find_element(By.LINK_TEXT, "SOLUTIONS").click()
+	WebDriverWait(browser, 1000)
+	browser.find_element(By.LINK_TEXT, "CONTACT").click()
+
+	if MAIL_TEST:
+		# WebDriverWait(browser, 1000)
+		logger.info("TESTING SEND MAIL")
+
+	WebDriverWait(browser, 10000)
+
+	browser.find_element(By.LINK_TEXT, "BLOG").click()
+	WebDriverWait(browser, 10000)
+	browser.find_element(By.LINK_TEXT, "HOA").click()
+	WebDriverWait(browser, 10000)
+
+	logger.info(f"Navigation Bar links tested. Mail test: {MAIL_TEST}")
+
+	return browser
+
+
+
+# print(a_refs.)
+
+# for ref in a_refs:
+# 	link = ref.get_attribute("href")
+#
+# 	if link == '' or '#' in link or 'mailto' in link or "vreasolutions" in link:
+# 		continue
+#
+# 	unique_links.add(link)
+#
+# with open(f"./output/links.txt", "w+") as fh:
+# 	for unique_link in unique_links:
+# 		fh.write(unique_link + '\n')
