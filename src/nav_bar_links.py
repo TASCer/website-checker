f4adf241ffaa07f09d420136bfd052d083e12ef6
TASCer <todd@tascs.net>
2024-01-19 13:35:55 -0700

    + try/except

--- src/nav_bar_links.py
+++ src/nav_bar_links.py
@@ -5,6 +5,8 @@ import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import ElementNotSelectableException
+
 
 now: dt = dt.date.today()
 todays_date: str = now.strftime('%D').replace('/', '-')
@@ -21,36 +23,50 @@ def browse(browser, MAIL_TEST: bool, site: str) -> object:
 		logger.error(e)
 
 	if MAIL_TEST and site == 'https://tascs.test':
-		# try to test if image/rnd value changes when refresh clicked to test
-		logger.info(f"HARD CODED ANSWER FOR captcha: {my_secrets.test_home_url}. Other method for PROD?")
+		logger.info("TESTING SEND MAIL FROM CONSULT FORM")
+		logger.info(f"**HARD CODED ANSWER FOR captcha: {my_secrets.test_home_url}**")
 
-		browser.find_element(By.ID, "refresh-captcha").click()
+		try:
 
-		fname = browser.find_element(By.NAME, 'firstname')
-		fname.send_keys("SELENIUM CONSULT")
-		WebDriverWait(browser, 1000)
+			browser.find_element(By.ID, "refresh-captcha").click()
 
-		lname = browser.find_element(By.NAME, 'lastname')
-		lname.send_keys("TESTER")
-		WebDriverWait(browser, 1000)
+			fname = browser.find_element(By.NAME, 'firstname')
+			fname.clear()
+			fname.send_keys("SELENIUM CONSULT")
+			WebDriverWait(browser, 1000)
 
-		email = browser.find_element(By.NAME, 'email')
-		email.send_keys("TESTER@CONSULTFORM.COM")
-		WebDriverWait(browser, 1000)
+			lname = browser.find_element(By.NAME, 'lastname')
+			lname.clear()
+			lname.send_keys("TESTER")
+			WebDriverWait(browser, 1000)
 
-		company = browser.find_element(By.NAME, 'company')
-		company.send_keys("SELENIUM CONSULT TESTING INC.")
-		WebDriverWait(browser, 1000)
+			email = browser.find_element(By.NAME, 'email')
+			email.clear()
+			email.send_keys("TESTER@CONSULTFORM.COM")
+			WebDriverWait(browser, 1000)
 
-		phone = browser.find_element(By.NAME, 'telephone')
-		phone.send_keys("1234567890")
-		WebDriverWait(browser, 1000)
+			company = browser.find_element(By.NAME, 'company')
+			company.clear()
+			company.send_keys("SELENIUM CONSULT TESTING INC.")
+			WebDriverWait(browser, 1000)
 
-		captcha = browser.find_element(By.NAME, 'captcha')
-		captcha.send_keys('17')
-		WebDriverWait(browser, 1000)
+			phone = browser.find_element(By.NAME, 'telephone')
+			phone.clear()
+			phone.send_keys("1234567890")
+			WebDriverWait(browser, 1000)
 
-		browser.find_element(By.NAME, 'submit').click()
+		except ElementNotSelectableException as e:
+			logger.error(e)
+
+		try:
+			captcha = browser.find_element(By.NAME, 'captcha')
+			captcha.send_keys('17')
+			WebDriverWait(browser, 1000)
+
+			browser.find_element(By.NAME, 'submit').click()
+			logger.info("CONSULT EMAIL SENT")
+		except Exception as e:
+			logger.error(e)
 
 	time.sleep(10)
 
@@ -58,33 +74,40 @@ def browse(browser, MAIL_TEST: bool, site: str) -> object:
 	WebDriverWait(browser, 1000)
 	browser.find_element(By.LINK_TEXT, "SOLUTIONS").click()
 	WebDriverWait(browser, 1000)
-	browser.find_element(By.LINK_TEXT, "CONTACT").click()
 
-	if MAIL_TEST:
+	browser.find_element(By.LINK_TEXT, "CONTACT").click()
+	if MAIL_TEST and site == 'https://tascs.test':
 		# WebDriverWait(browser, 1000)
-		logger.info("TESTING SEND MAIL")
+		logger.info("TESTING SEND MAIL FROM CONTACT FORM")
 		name = browser.find_element(By.NAME, 'name')
+		name.clear()
 		name.send_keys("SELENIUM CONTACT TEST")
 		WebDriverWait(browser, 1000)
 
 		email = browser.find_element(By.NAME, 'email')
+		email.clear()
 		email.send_keys("TESTER@CONTACTFORM.COM")
 		WebDriverWait(browser, 1000)
 
 		company = browser.find_element(By.NAME, 'company')
+		company.clear()
 		company.send_keys("SELENIUM CONTACT TESTING INC")
 		WebDriverWait(browser, 1000)
 
 		phone = browser.find_element(By.NAME, 'telephone')
+		phone.clear()
 		phone.send_keys("1234567890")
 		WebDriverWait(browser, 1000)
 
 		comments = browser.find_element(By.NAME, 'message')
+		comments.clear()
 		comments.send_keys("SELENIUM CONTACT TESTING")
 		WebDriverWait(browser, 1000)
 
 		browser.find_element(By.ID, 'submit-form').click()
 		WebDriverWait(browser, 1000)
+
+		logger.info("CONTACT EMAIL SENT")
 
 	WebDriverWait(browser, 1000)
 
