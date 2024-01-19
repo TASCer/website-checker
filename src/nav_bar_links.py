55ea9c46a7328d3886fa5217b4ca91a2e598fab9
TASCer <todd@tascs.net>
2024-01-19 12:31:45 -0700

    test has captcha const ans

--- src/nav_bar_links.py
+++ src/nav_bar_links.py
@@ -12,18 +12,48 @@ todays_date: str = now.strftime('%D').replace('/', '-')
 logger = logging.getLogger(__name__)
 
 
-def browse(browser, MAIL_TEST):
-	logger.info("Testing navigation bar links")
-	browser.get(my_secrets.test_consult_url)
-	WebDriverWait(browser, 1000)
-	if MAIL_TEST and my_secrets.test_home_url:
+def browse(browser, MAIL_TEST: bool, site: str) -> object:
+	try:
+		browser.get(site)
+		WebDriverWait(browser, 1000)
+		logger.info(f"Testing navigation bar links for: {site}. Email test: {MAIL_TEST}")
+	except Exception as e:
+		logger.error(e)
+
+	if MAIL_TEST and site == 'https://tascs.test':
 		# try to test if image/rnd value changes when refresh clicked to test
-		logger.info(f"NEED HARD CODE VALUE OR ?? for captcha: {my_secrets.test_home_url}")
+		logger.info(f"HARD CODED ANSWER FOR captcha: {my_secrets.test_home_url}. Other method for PROD?")
+
 		browser.find_element(By.ID, "refresh-captcha").click()
-		browser.find_element(By.ID, "refresh-captcha").click()
-		time.sleep(10)
-		browser.find_element(By.ID, "refresh-captcha").click()
-		time.sleep(5)
+
+		fname = browser.find_element(By.NAME, 'firstname')
+		fname.send_keys("SELENIUM CONSULT")
+		WebDriverWait(browser, 1000)
+
+		lname = browser.find_element(By.NAME, 'lastname')
+		lname.send_keys("TESTER")
+		WebDriverWait(browser, 1000)
+
+		email = browser.find_element(By.NAME, 'email')
+		email.send_keys("TESTER@CONSULTFORM.COM")
+		WebDriverWait(browser, 1000)
+
+		company = browser.find_element(By.NAME, 'company')
+		company.send_keys("SELENIUM CONSULT TESTING INC.")
+		WebDriverWait(browser, 1000)
+
+		phone = browser.find_element(By.NAME, 'telephone')
+		phone.send_keys("1234567890")
+		WebDriverWait(browser, 1000)
+
+		captcha = browser.find_element(By.NAME, 'captcha')
+		captcha.send_keys('17')
+		WebDriverWait(browser, 1000)
+
+		browser.find_element(By.NAME, 'submit').click()
+
+	time.sleep(10)
+
 	browser.find_element(By.LINK_TEXT, "WHY TASCS?").click()
 	WebDriverWait(browser, 1000)
 	browser.find_element(By.LINK_TEXT, "SOLUTIONS").click()
