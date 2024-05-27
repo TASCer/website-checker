011188dc7b676d125c79a55ed255fc8e772350d6
TASCer <todd@tascs.net>
2024-05-27 09:59:32 -0700

    working on tls

--- src/mailer.py
+++ src/mailer.py
@@ -3,13 +3,15 @@ import datetime as dt
 import logging
 import my_secrets
 import smtplib
+import ssl
 
 from datetime import datetime
 from email import encoders
 from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from logging import Logger
+
 
 now: datetime = dt.datetime.now()
 todays_date: str = now.strftime('%D').replace('/', '-')
@@ -75,26 +77,22 @@ def send_mail(subject: str, attachment_path: object = None):
         part_basic: MIMEText = MIMEText(html_basic, "html")
         msg.attach(part_basic)
 
-    with smtplib.SMTP(my_secrets.postfix_mailhost, 25) as server:
-        try:
-            server.sendmail(sender_email, receiver_email, msg.as_string())
-            # logger.info("email sent")
-        except smtplib.SMTPException as e:
-            logger.exception(f"email not sent {str(e)}")
-
-
-# send_mail("TEST FROM SELENIUM")
-
-
-
+    # WORKING NON SSL 25 or 587
+    # with smtplib.SMTP(my_secrets.postfix_mailhost, 25) as server:
+    #     try:
+    #         server.sendmail(sender_email, receiver_email, msg.as_string())
+    #
+    #     except smtplib.SMTPException as e:
+    #         logger.exception(f"email not sent {str(e)}")
 
     # #################################### SSL TESTING
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)   # ssl.create_default_context
+    context = ssl.create_default_context(ssl.PROTOCOL_TLS_CLIENT)   # ssl.create_default_context
     # context.set_ciphers('TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384')        #("TLS_RSA_WITH_AES_128_CBC_SHA256")     # ("TLS_DHE_RSA_WITH_AES_128_GCM_SHA256")
     # context.hostname_checks_common_name = False
     # context.check_hostname = False
     # context.verify_mode = ssl.CERT_NONE
-    # ser_cert = ssl.get_server_certificate(my_secrets.exchange_mailhost, 25)
+    # ser_cert = ssl.get_server_certificate((my_secrets.postfix_mailhost, 587))
+    # print(ser_cert)
 #     context.load_default_certs()
 #     ca = context.get_ca_certs()
 #     c = context.get_ciphers()
@@ -110,20 +108,21 @@ def send_mail(subject: str, attachment_path: object = None):
 #         # for d in c:
 #         #     print(d)
 #         #     print(type(d))
-#     try:
-#         with smtplib.SMTP_SSL(my_secrets.exchange_mailhost, 587, context=context) as server:
-#             server.login(my_secrets.exchange_user, my_secrets.exchange_password)  # NTLM issue? wrong version issue .997?
-#             server.ehlo("tascslt")
-#             server.starttls()
-#             server.sendmail(my_secrets.mail_from, receiver_email, msg.as_string())
-#
-#     except smtplib.SMTPException as e:
-#         print("SMTPERROR",e)
-#     except ssl.SSLError as e:
-#         print("SSLError", str(e))
-#     except ssl.ALERT_DESCRIPTION_HANDSHAKE_FAILURE as e:
-#         print(e)
-#     except ssl.SSLCertVerificationError as e:
-#         print(e)
-#
-# send_mail("hello, NON TLS test to rpi4 on port 25. Shows no date!?")
+    try:
+        with smtplib.SMTP_SSL(my_secrets.postfix_mailhost, 25, context=context) as server:
+            server.login(user=my_secrets.postfix_user, password=my_secrets.postfix_password)  # NTLM issue? wrong version issue .997?
+            server.ehlo("tascslt.tascs.local")
+            server.starttls(context=context)
+            server.sendmail(my_secrets.postfix_mail_from, receiver_email, msg.as_string())
+
+    except smtplib.SMTPException as e:
+        print("SMTPERROR", e)
+    except ssl.SSLCertVerificationError as e:
+        print(e)
+    except ssl.SSLError as e:
+        print("SSLError", str(e))
+    except ssl.ALERT_DESCRIPTION_HANDSHAKE_FAILURE as e:
+        print(e)
+
+
+send_mail("hello, TLS test on port 587")
