77c7061d4907f490f8fede15ba66a470f35b787d
TASCer <todd@tascs.net>
2024-06-26 13:18:38 -0700

    secrets template

---
+++ misc/template_my_secrets.py
@@ -0,0 +1,28 @@
+# PROD  tascs.net
+prod_contact_url = 'prod/url/to/contact/form'
+prod_consult_url = 'prod/url/to/consult/form'  #CAPtCHA
+prod_home_url = prod_consult_url
+# INTRANET  tascs.local
+local_contact_url = 'local/url/to/contact/form'
+local_consult_url = 'local/url/to/consult/form'  #CAPtCHA
+local_home_url = local_consult_url
+# TEST tascs.test
+test_contact_url = 'test/url/to/contact/form'
+test_consult_url = 'test/url/to/consult/form'  #CAPtCHA
+test_home_url = test_consult_url
+# FF webdriver
+firefox_driver = r"D:\selenium_firefox_driver\geckodriver.exe"
+# CHROME webdriver
+chrome_driver = r"D:\selenium_chrome_driver\chromedriver.exe"
+# PROD EMAIL INFO
+exchange_user = "email server username"
+exchange_password = "email server password"
+exchange_mail_from = "xxx@xxxx.xxx"
+exchange_mailhost = 'email server FQDN'
+# RECIPIENT LIST
+email_to = ['ccc@ccc.ccc', 'cccc@cccc.ccc']
+# TESTS EMAIL INFO
+postfix_user = "email server username"
+postfix_password = "email server password"
+postfix_mail_from = "xxx@dom.tld"
+postfix_mailhost = 'email server FQDN'
