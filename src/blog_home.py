3462774bc3f5d7cf92f924c4b9405b0bf8a634b3
TASCer <todd@tascs.net>
2024-07-06 10:46:36 -0700

    + latest blog title

--- src/blog_home.py
+++ src/blog_home.py
@@ -14,13 +14,14 @@ def browse(browser, site: str) -> object:
     try:
         browser.get(site)
         WebDriverWait(browser, 1000)
-        logger.info(f"\tBlog posts: {site}")
+        logger.info(f"\tTesting Blog page for: {site}")
 
     except Exception as e:
         logger.error(e)
 
     blog_entries = browser.find_elements(By.CLASS_NAME, "entry-title")
     blog_titles = [e.text for e in blog_entries]
-    logger.info(f"\tFirst Page Blog Count: {len(blog_titles)}")
+    logger.info(f"\t\tFirst Page Blog Count: {len(blog_titles)}")
+    logger.info(f"\t\tLatest Blog Title: {blog_titles[0]}")
 
     return browser
