0bc48a53f735f8234e617527c44213afbd8d60cf
TASCer <todd@tascs.net>
2024-11-07 09:59:32 -0700

    working, trying to get lps rental %

--- src/hoa_home.py
+++ src/hoa_home.py
@@ -4,7 +4,7 @@ import create_browser
 
 from my_secrets import test_home_url
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 
 now: dt = dt.date.today()
 todays_date: str = now.strftime("%D").replace("/", "-")
@@ -12,7 +12,15 @@ todays_date: str = now.strftime("%D").replace("/", "-")
 logger = logging.getLogger(__name__)
 
 
-def browse(browser, site: str) -> object:
+def browse(browser, site: str) -> tuple[str]:
+    """
+    Function vistits the HOA community map to get and return
+    -Last db data timestamp
+    -Area rental percentage
+    :param browser:
+    :param site:
+    :return:
+    """
     try:
         browser.get(site + "/hoa/areaMap.php")
         WebDriverWait(browser, 1000)
@@ -21,14 +29,14 @@ def browse(browser, site: str) -> object:
     except Exception as e:
         logger.error(e)
 
-    last_db_update = browser.find_element(By.ID, "TS")
-    area_rental_percent = browser.find_element(By.ID, "TPCT")
+    last_db_update = browser.find_element(By.ID, "TS").text
+    area_rental_percent = browser.find_element(By.ID, "TPCT").text
+    select = Select(browser.find_element(By.ID, "community-names"))
+    print(select)
+    select_lps = select.first_selected_option
+    print(select_lps.text)
 
-    return last_db_update.text, float(area_rental_percent.text)
-
-    # TODO TESTING CLICKING LPS FROM DROPDPWN
-    # drop = browser.find_element(By.ID, "community-names").click()
-    # print(drop)
+    return last_db_update, area_rental_percent
 
 # GET LPS RENTAL AVG - WORKS. Add to above? NO browser/url issue
 #     try:
