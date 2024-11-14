d987b8e6a97c45f4f5a01742fdc848dde5107000
TASCer <todd@tascs.net>
2024-11-14 07:51:02 -0700

    created new conda env

--- misc/launch-selenium.bat
+++ misc/launch-selenium.bat
@@ -1,5 +1,5 @@
 cd /D "D:\PycharmProjects\Selenium\src"
 echo %cd%
-call conda activate Selenium
+call conda activate website-checker311
 python.exe "main.py" %1
 if NOT ["%errorlevel%"] == ["0"] pause
