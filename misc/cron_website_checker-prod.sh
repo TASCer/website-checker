a9955934be541a2e300aaeae76e677fc761648f3
Todd S <info@tascs.net>
2025-03-04 11:47:03 -0700

    testing prod and test weekly

---
+++ misc/cron_website_checker-prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/todd/python_projects/website-checker/src
+
+source /home/todd/python_projects/website-checker/.venv/bin/activate
+
+/home/todd/.local/bin/uv run main.py prod
+
+
