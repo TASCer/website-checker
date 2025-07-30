a47aa933c67bf7bfff4a9d694a9e734d4311974b
Todd S <info@tascs.net>
2025-07-30 08:15:43 -0700

    renamed

---
+++ src/test-manager.py
@@ -0,0 +1,14 @@
+# https://linuxiac.com/how-to-install-google-chrome-on-debian-12-bookworm/
+# selenium 4
+# selenium 4
+from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.core.os_manager import ChromeType
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+# driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+hoa = driver.get("https://hoa.tascs.test")
+print(hoa)
--- src/test.py
+++
@@ -1,8 +0,0 @@
-# https://linuxiac.com/how-to-install-google-chrome-on-debian-12-bookworm/
-# selenium 4
-# selenium 4
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
